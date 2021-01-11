b7f1a96a87a489029a0e43698a3515aea4469eea
Jan Schulte <jan@work-with-jan.com>
2021-01-11 08:36:28 -0500

    Add CLI library

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const fs = require('fs');
 const fetch = require('node-fetch');
 const TurndownService = require('turndown');
+const { program } = require('commander');
+program.version('0.0.1');
+
+program
+  .requiredOption('-w, --website <url>', 'website to fetch and convert')
+  .option('-f, --filename <filename>', 'Custom filename for output file');
+
+program.parse(process.argv);
+
+const filename = program.filename ? program.filename : "website.md";
 
 const turndownService = new TurndownService();
 
-fetch('https://github.com/')
+fetch(program.website)
     .then(res => res.text())
     .then(body => {
         const markdown = turndownService.turndown(body);
-        console.log(markdown)
+        return Promise.resolve(markdown);
+    })
+    .then(markdown => {
+        fs.writeFile(filename, markdown, function (err) {
+            if (err) return Promise.reject(err);
+            console.log('Wrote file.');
+        });
     })
     .catch((err) => console.error(err));
